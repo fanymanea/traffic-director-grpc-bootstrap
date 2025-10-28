77d4aa490552e9f0bcaefdde6c2448ff9fd6b0f2
Stefan Manea <stmanea@google.com>
2025-10-28 22:05:58 +0000

    Add the experimental suffix to the new flag

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ var (
 	generateMeshID             = flag.Bool("generate-mesh-id", false, "When enabled, the CSM MeshID is generated. If config-mesh flag is specified, this flag would be ignored. Location and Cluster Name would be retrieved from the metadata server unless specified via gke-location and gke-cluster-name flags respectively.")
 	includeAllowedGrpcServices = flag.Bool("include-allowed-grpc-services-experimental", false, "When enabled, generates `allowed_grpc_services` map that includes current xDS Server URI. This flag is EXPERIMENTAL and may be changed or removed in a later release.")
 	isTrustedXDSServer         = flag.Bool("is-trusted-xds-server-experimental", false, "Whether to include the server feature trusted_xds_server for TD. This flag is EXPERIMENTAL and may be changed or removed in a later release.")
-	xdsServerRegion            = flag.String("xds-server-region", "", "The Cloud region (e.g. 'us-central1') of the xDS server to be used for regional setup.")
+	xdsServerRegion            = flag.String("xds-server-region-experimental", "", "The Cloud region (e.g. 'us-central1') of the xDS server to be used for regional setup.")
 )
 
 const (
